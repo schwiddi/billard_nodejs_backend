8a89e954719ee69ecd5adf257e2c6a5d3e667503
schwiddi <schwiddi@me.com>
2018-08-09 22:49:10 +0200

    some stuff

---
+++ index.js
@@ -0,0 +1,28 @@
+// hol dir express
+const express = require('express');
+// das gibt ne funktion zurück also schreibe ich es in eine const
+
+// gib hier mal express aus
+console.log(express);
+
+// doku --> http://expressjs.com/de/4x/api.html
+
+// nun nutze diese funktionen als app, das ist so konvention offensichtlich
+const app = express();
+// hier führst du express() aus, die klammern sagen ausführen heissen irgendwie parasynthese
+// dann schreibst du den output in die const app
+
+// gebe mal die app aus, um zu schauen was ide alles für funktionen oder methoden hat wie auch immer..
+console.log('##############################################');
+console.log('##############################################');
+console.log('##############################################');
+console.log('##############################################');
+console.log('##############################################');
+console.log('##############################################');
+console.log(app);
+
+
+
+app.listen(3001, () => {
+    console.log('listener started on 3001...')
+});
