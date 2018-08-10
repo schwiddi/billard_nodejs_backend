b5441f7727c3dc2e58948807a24eea3af9a3afc7
schwiddi <schwiddi@me.com>
2018-08-11 00:13:56 +0200

    bla

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const express = require('express');
 // das gibt ne funktion zurück also schreibe ich es in eine const
 
 // gib hier mal express aus
-console.log(express);
+//console.log(express);
 
 // doku --> http://expressjs.com/de/4x/api.html
 
 // nun nutze diese funktionen als app, das ist so konvention offensichtlich
-const app = express();
+//const app = express();
 // hier führst du express() aus, die klammern sagen ausführen heissen irgendwie parasynthese
 // dann schreibst du den output in die const app
 
 // gebe mal die app aus, um zu schauen was ide alles für funktionen oder methoden hat wie auch immer..
-console.log('##############################################');
-console.log(app);
+//console.log('##############################################');
+//console.log(app);
 
 
 
