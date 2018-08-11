ed6f96f94f6e970ac159699c8ac83a72b30cf278
schwiddi <schwiddi@me.com>
2018-08-11 22:19:06 +0200

    some port things

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 /*jshint esversion: 6 */
 const express = require('express');
 const app = express();
-const port = '3001';
+const fallbackport = '3002';
 
 // define routes
 app.get('/', (req, res) => {
-    res.send('Hello..');
+    res.send('<h1>Hello World..</h1>');
     console.log('hey i got a request.. yay..');
 });
 
 app.get('/api/v1/games', (req,res) => {
     res.send([1, 2, 3]);
+    console.log('someone used your api');
 });
 
+
+
+// Getting PORT form env var
+const port = process.env.PORT || fallbackport;
 
 // starting server stuff
 app.listen(port);
