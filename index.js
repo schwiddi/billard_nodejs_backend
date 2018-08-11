e6e2f118986172d1a0845d542320df8aebfcd8bd
schwiddi <schwiddi@me.com>
2018-08-11 19:34:20 +0200

    some http tests

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const express = require('express'); // doku http://expressjs.com/de/4x/api.html
+/*jshint esversion: 6 */
+// const express = require('express'); // doku http://expressjs.com/de/4x/api.html
+const http = require('http');
+// const app = express();
+// const port = 3001
 
-const app = express();
+// app.listen(port, () => {
+    // console.log(`listener started on ${port}...`)
+// });
 
-const port = 3001
 
-app.listen(port, () => {
-    console.log(`listener started on ${port}...`)
+
+const myserver = http.createServer((req, res) => {
+    if (req.url === '/') {
+        console.log('got connection')
+        res.write('hello you');
+        res.end();
+    }
 });
+
+myserver.listen(3002);
