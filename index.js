5e04ab0124703515db70068eb9744ae9be2c4c18
schwiddi <schwiddi@me.com>
2018-08-11 23:07:43 +0200

    cleaned

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
 /*jshint esversion: 6 */
 const express = require('express');
 const app = express();
-const fallbackport = '3002';
+const fallbackport = '3001';
+// Getting PORT form env var
+const port = process.env.PORT || fallbackport;
 const games = [
     { id: 1, playerA: 'Schwiddi', playerB: 'Basil', scorePlayerA: 1, scorePlayerB: 0 },
     { id: 2, playerA: 'Schwiddi', playerB: 'Basil', scorePlayerA: 0, scorePlayerB: 1 },
-    { id: 3, playerA: 'Mani', playerB: 'Basil', scorePlayerA: 0, scorePlayerB: 1 }
+    { id: 3, playerA: 'Mani', playerB: 'Basil', scorePlayerA: 0, scorePlayerB: 1 },
+    { id: 4, playerA: 'Hazem', playerB: 'Schwiddi', scorePlayerA: 1, scorePlayerB: 0 }, 
+    { id: 5, playerA: 'Schwiddi', playerB: 'Hazem', scorePlayerA: 0, scorePlayerB: 1 },
 ];
 
 // define routes
+// root route
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World..</h1><p>use /api/v1/games...');
+    res.send('<h1>Hello World..</h1><p><a href="api/v1/games">use api/v1/games ...</a></p>');
     console.log('someone has come to the root of your node..');
 });
 
+// get all games
 app.get('/api/v1/games', (req, res) => {
     res.send(games);
     console.log('someone used your games api');
 });
 
+// get game by id
 app.get('/api/v1/games/:id', (req, res) => {
+    // games.find is standard function for arrays
+    // for c look if c.id equals req.params.id
+    // and cause of this is a string not an int
+    // we need to parse it via global function parseInt
     const game = games.find(c => c.id === parseInt(req.params.id));
     res.send(game);
-    const wantedid = req.params.id;
-    console.log(`someone looked for the game with the id ${wantedid}`);
+    console.log(`someone looked for the game with the id ${game}`);
 });
 
 
-// Getting PORT form env var
-const port = process.env.PORT || fallbackport;
+
 
 // starting server stuff
 app.listen(port);
