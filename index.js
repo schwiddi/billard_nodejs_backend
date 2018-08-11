12bf782b05f43152605a46853eb545addcd7437d
schwiddi <schwiddi@me.com>
2018-08-11 22:06:31 +0200

    more

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 /*jshint esversion: 6 */
 const express = require('express');
 const app = express();
-const port = '3001'
+const port = '3001';
 
+// define routes
 app.get('/', (req, res) => {
     res.send('Hello..');
-    console.log('hey i got a request.. yay..')
+    console.log('hey i got a request.. yay..');
+});
+
+app.get('/api/v1/games', (req,res) => {
+    res.send([1, 2, 3]);
 });
 
 
-app.listen(port)
-console.log(`started listener on port ${port}`)
+// starting server stuff
+app.listen(port);
+console.log(`started listener on port ${port}`);
