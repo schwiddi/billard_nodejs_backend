e8c274b7c3ef6322fa10a96c594f5595fc000465
schwiddi <schwiddi@me.com>
2018-08-14 20:10:32 +0200

    added some more debug and moved the mongodb connection in here

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 // Hints
 /*jshint esversion: 6 */
 
+
 // Import things
-const express = require('express');             // middleware
+const express = require('express');                   // middleware
 const gamesrouter = require('./routes/games');        // my route to games handler    
 const rootrouter = require('./routes/root');          // my route to /
 const debuginit = require('debug')('app:init');
+const debugdb = require('debug')('app:db');
+const mongoose = require('mongoose');
+debuginit('Require Section done.');
+
 
 // ENV things
 const fallbackport = '3001';
 const port = process.env.PORT || fallbackport;
+debuginit('Environment Settings Done.');
+
 
 // setting up express
 const app = express();                  // create object app from express
+debuginit('Express started.');
 app.use(express.json());                // configure the object to handle json
 app.use('/api/v1/games', gamesrouter);  // say middleware to use for this route the const that was importet before
 app.use('/', rootrouter);               // use the router to route /
+debuginit('Express setup done.');
+
+
+// Mongoose setup
+debugdb('Setting up MongoDB connection...');
+mongoose.connect('mongodb://localhost/r21billard')
+    .then(() => debugdb('Connection to MongoDB done.'))
+    .catch(err => debugdb('Having problems connecting to DB!!!', err));
+
 
 // starting server stuff
 app.listen(port);
