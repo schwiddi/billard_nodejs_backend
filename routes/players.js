9f4cdda00a7124759ba43759fe50ecea8214383c
schwiddi <schwiddi@me.com>
2018-08-17 21:55:00 +0200

    cleanup

--- routes/players.js
+++ routes/players.js
@@ -3,10 +3,8 @@
 
 // Import things
 const express = require('express'); // middleware
-const Joi = require('joi'); // validation
 const debugplayers = require('debug')('app:players');
 const Game = require('../db/mongo_connector');
-const mongoose = require('mongoose');
 
 // ENV things
 
@@ -26,19 +24,23 @@ const router = express.Router(); // create object
 // }
 
 router.get('/', async (req, res) => {
+  debugplayers('get all games');
   const games = await Game.find().select({
     playerA: 1,
     playerB: 1
   });
+  debugplayers('all games stored');
 
   let allplayers = [];
 
   let length = games.length;
 
+  debugplayers('push all players to array');
   for (i = 0; i < length; i++) {
     allplayers.push(games[i].playerA);
     allplayers.push(games[i].playerB);
   }
+  debugplayers('all players stored');
 
   let playersdistinct = [];
 
