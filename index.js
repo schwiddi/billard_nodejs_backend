fc52356cfaa05492eae7384a640ef0664fe91995
schwiddi <schwiddi@me.com>
2018-08-20 09:49:15 +0200

    cleanup

--- index.js
+++ index.js
@@ -8,36 +8,19 @@ const playersrouter = require('./routes/players');
 const rootrouter = require('./routes/root'); // my route to /
 const mydebug = require('./common/mydebug');
 const mongoose = require('mongoose');
-mydebug('Require Section done.');
 
 // ENV things
 const fallbackport = '3001';
 const port = process.env.PORT || fallbackport;
-mydebug('Environment Settings Done.');
 
 // setting up express
 const app = express(); // create object app from express
-mydebug('Express started.');
-
-// trying to use some middleware req logs
-app.use(function(req, res, next) {
-  mydebug(`middleware got req!`);
-  mydebug(`req.ip: ${req.ip}`);
-  mydebug(`req.method: ${req.method}`);
-  mydebug(`req.originalUrl: ${req.originalUrl}`);
-  mydebug(`req.path: ${req.path}`);
-  next();
-});
-
-// other middleware stuff
 app.use(express.json()); // configure the object to handle json
 app.use('/api/v1/games', gamesrouter); // say middleware to use for this route the const that was importet before
 app.use('/api/v1/players', playersrouter);
 app.use('/', rootrouter); // use the router to route /
-mydebug('Express setup done.');
 
 // Mongoose setup
-mydebug('Setting up MongoDB connection...');
 mongoose
   .connect(
     'mongodb://localhost:27017/r21billard',
@@ -50,6 +33,5 @@ mongoose
   });
 
 // starting server stuff
-mydebug('starting listener...');
 app.listen(port);
 mydebug(`Listener started on ${port}`);
