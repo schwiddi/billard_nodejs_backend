5470cda8f5ff0591b4ddbf484177677297585644
schwiddi <schwiddi@me.com>
2018-09-10 17:32:58 +0200

    added connection interval to keep alive mysql session

--- db/db_connection.js
+++ db/db_connection.js
@@ -4,9 +4,15 @@
 // Import things
 const mysql = require('mysql');
 const dbconfig = require('./db_conf');
+const mydebug = require('../common/mydebug');
 
 // ENV things
 const db = mysql.createConnection(dbconfig);
+
+setInterval(function() {
+  db.query('SELECT 1');
+  mydebug('did req to db to keep session alive');
+}, 60000);
 
 // now Export this Model
 module.exports = db;
