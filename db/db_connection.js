6dcbeb6d05e9ce19d8f575d02d7dc382f892535b
schwiddi <schwiddi@me.com>
2018-09-14 13:10:23 +0200

    raised interval

--- db/db_connection.js
+++ db/db_connection.js
@@ -12,7 +12,7 @@ const db = mysql.createConnection(dbconfig);
 setInterval(function() {
   db.query('SELECT 1');
   mydebug('did req to db to keep session alive');
-}, 60000);
+}, 600000);
 
 // now Export this Model
 module.exports = db;
