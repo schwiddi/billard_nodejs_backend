10ba7400706a68b1f7464044506d4223d2aa9e70
Dominik Schwitter <dominik.schwitter@postfinance.ch>
2018-09-19 17:34:47 +0200

    added users endpoint

---
+++ routes/users.js
@@ -0,0 +1,52 @@
+// Hints
+/*jshint esversion: 6 */
+
+// Import things
+const db = require('../db/db_connection');
+const express = require('express');
+const mydebug = require('../common/mydebug');
+const _ = require('underscore');
+const Joi = require('joi');
+
+// setting up express
+const router = express.Router();
+
+
+// Input Validation for Users
+function validateUser(user) {
+    const schema = {
+        name: Joi.string().min(2).max(255).required(),
+        email: Joi.string().email({ minDomainAtoms: 2}).max(255).required(),
+        password: Joi.string().min(3).max(1024).required()
+    };
+    return Joi.validate(user, schema);
+  }
+
+// write new user = register
+router.post('/', async (req, res) => {
+    const { error } = validateUser(req.body);
+    if (error) {
+      mydebug(`joi user input validation was nok`);
+      return res.status(400).send(error.details[0].message);
+    }
+  
+    const sp = `CALL AddUser(
+      '${req.body.name}',
+      '${req.body.email}',
+      '${req.body.password}')`;
+  
+    db.query(sp, true, (error, results, fields) => {
+      if (error) {
+        mydebug(error.message);
+        return res.status(404).send('something went wrong..');
+      } else if (_.isEmpty(results[0])) {
+        mydebug(`returned inserted user from db was empty`);
+        return res.status(404).send('something went wrong..');
+      } else {
+        mydebug('user added');
+        return res.send(results[0]);
+      }
+    });
+  });
+
+module.exports = router;
