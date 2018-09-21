3fa7157aecf94b1a26f5937fb2a37b7fcf2fa22a
schwiddi <schwiddi@me.com>
2018-09-21 19:30:30 +0200

    added auth

--- routes/auth.js
+++ routes/auth.js
@@ -8,59 +8,76 @@ const mydebug = require('../common/mydebug');
 const _ = require('underscore');
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 // setting up express
 const router = express.Router();
 
 function validate(req) {
-    const schema = {
-        email: Joi.string().email({ minDomainAtoms: 2}).max(255).required(),
-        password: Joi.string().min(3).max(1024).required()
-    };
-    return Joi.validate(req, schema);
-  }
+  const schema = {
+    email: Joi.string()
+      .email({ minDomainAtoms: 2 })
+      .max(255)
+      .required(),
+    password: Joi.string()
+      .min(3)
+      .max(1024)
+      .required()
+  };
+  return Joi.validate(req, schema);
+}
+
+async function bcryptCompare(input, database) {
+  return bcrypt.compare(input, database);
+}
 
 router.post('/', async (req, res) => {
-    const { error } = validate(req.body);
+  const { error } = validate(req.body);
+  if (error) {
+    mydebug(`joi auth validation was nok`);
+    return res.status(400).send(error.details[0].message);
+  }
+
+  let sql = `CALL CheckIfUserExist('${req.body.email}')`;
+  db.query(sql, true, (error, results, fields) => {
     if (error) {
-      mydebug(`joi user input validation was nok`);
-      return res.status(400).send(error.details[0].message);
+      mydebug(error.message);
+      return res.status(500).send('something went wrong..');
+    } else if (_.isEmpty(results[0])) {
+      mydebug(`auth with unknown user!!!`);
+      return res.status(400).send('mail or passwort wrong');
     }
-
-    // generate hashed pw here cause it needs async and i dont know how to do that inside de db if statement
-    // const salt = await bcrypt.genSalt(10);
-    // const hashedpw = await bcrypt.hash(req.body.password, salt)
-
-
-    // const checkifmailalreadyregistered = `CALL CheckMailAlreadyRegistered('${req.body.email}')`;
-
-    // db.query(checkifmailalreadyregistered, true, (error, results, fields) => {
-    //   if (error) {
-    //     mydebug(error.message);
-    //     return res.status(500).send('something went wrong on the backend...');
-    //   } else if (_.isEmpty(results[0])) {
-    //     const sp = `CALL AddUser(
-    //       '${req.body.name}',
-    //       '${req.body.email}',
-    //       '${hashedpw}')`;
-      
-    //     db.query(sp, true, (error, results, fields) => {
-    //       if (error) {
-    //         mydebug(error.message);
-    //         return res.status(500).send('something went wrong on the backend...');
-    //       } else if (_.isEmpty(results[0])) {
-    //         mydebug(`returned inserted user from db was empty`);
-    //         return res.status(500).send('something went wrong on the backend...');
-    //       } else {
-    //         mydebug('user added');
-    //         return res.send(results[0]);
-    //       }
-    //     });
-    //   } else {
-    //     mydebug('mail already registered');
-    //     return res.status(400).send('e-mail already registered...');
-    //   }
-    // });
   });
+
+  sql = `CALL GetHashedPassword('${req.body.email}')`;
+  db.query(sql, true, (error, results, fields) => {
+    if (error) {
+      mydebug(error.message);
+      return res.status(500).send('something went wrong..');
+    } else if (_.isEmpty(results[0])) {
+      mydebug(`password empty in database?!!!`);
+      return res.status(500).send('something went wrong..');
+    } else {
+      const sqlstring = JSON.stringify(results[0]);
+      const jsonstring = JSON.parse(sqlstring);
+      const dbpw = jsonstring[0]['password'];
+      const compareResult = bcryptCompare(req.body.password, dbpw);
+      console.log(compareResult);
+      if (!compareResult) return res.status(400).send('mail or passwort wrong');
+    }
+  });
+
+  // set logged in field in db
+  sql = `CALL SetLastLogin('${req.body.email}')`;
+  db.query(sql, true, (error, results, fields) => {
+    if (error) {
+      mydebug(error.message);
+    }
+  });
+
+  // jwt stuff
+  const token = jwt.sign({ email: req.body.email }, 'testkey');
+  res.status(200).send(token);
+});
 
 module.exports = router;
