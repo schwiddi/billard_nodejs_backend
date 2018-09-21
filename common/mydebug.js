74e425d41581a951235bb30711f52ca51bb10257
schwiddi <schwiddi@me.com>
2018-09-21 19:30:24 +0200

    added time to debug

--- common/mydebug.js
+++ common/mydebug.js
@@ -2,7 +2,21 @@
 /*jshint esversion: 6 */
 
 // Import things
-const mydebug = require('debug')('mydebug:all');
+var currentdate = new Date();
+var datetime =
+  currentdate.getDate() +
+  '.' +
+  (currentdate.getMonth() + 1) +
+  '.' +
+  currentdate.getFullYear() +
+  ' ' +
+  currentdate.getHours() +
+  ':' +
+  currentdate.getMinutes() +
+  ':' +
+  currentdate.getSeconds();
+
+const mydebug = require('debug')(`mydebug@${datetime}`);
 
 // now Export this Model
 module.exports = mydebug;
