7cc2ceda478d5df55c886944d41858d1536c1362
schwiddi <schwiddi@me.com>
2018-09-30 01:16:52 +0200

    switched so that the caller decide what todo

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,15 +4,15 @@ process.env.JWTKEY = 'tmp';
 module.exports = function auth(req, res, next) {
   const token = req.header('x-auth-token');
   if (!token)
-    return res.status(401).send('you dont have enough privileges buddy...');
+    return res.status(401).send('you dont have enough privileges buddy......');
 
   try {
     const jwtpayload = jwt.verify(token, process.env.JWTKEY);
-    if (jwtpayload.canAddGame == 1) {
+    if (jwtpayload) {
       req.user = jwtpayload;
       next();
     } else {
-      res.status(401).send('you dont have enough privileges buddy...');
+      res.status(400).send('hm...');
     }
   } catch (ex) {
     res.status(400).send('invalid token...');
