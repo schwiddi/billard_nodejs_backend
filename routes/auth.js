0de916c2f26cfc7d9a1836fbeea9ea5d090894df
schwiddi <schwiddi@me.com>
2018-09-29 22:00:58 +0200

    added auth

---
+++ routes/auth.js
@@ -0,0 +1,87 @@
+// Hints
+/*jshint esversion: 6 */
+
+// Import things
+const db = require('../db/db_connection');
+const express = require('express');
+const mydebug = require('../common/mydebug');
+const _ = require('underscore');
+const Joi = require('joi');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// setting up express
+const router = express.Router();
+
+// private jwt
+process.env.JWTKEY = 'tmp';
+
+// Input Validation for Users
+function validate(req) {
+  const schema = {
+    email: Joi.string()
+      .email({ minDomainAtoms: 2 })
+      .max(255)
+      .required(),
+    password: Joi.string()
+      .min(3)
+      .max(1024)
+      .required()
+  };
+  return Joi.validate(req, schema);
+}
+
+router.post('/', function(req, res) {
+  try {
+    let { error: err } = validate(req.body);
+    if (err) {
+      mydebug(`joi input validation was nok`);
+      // sende extra diese meldung und nich joi feedback
+      return res.status(400).send('email or password wrong');
+    }
+
+    let email = req.body.email;
+    let reqpw = req.body.password;
+
+    let sp = `CALL GetUser('${email}')`;
+
+    db.query(sp, function(err, results, fields) {
+      if (err) {
+        mydebug(err.message);
+        return res.status(500).send('something went wrong on the backend...');
+      } else if (results[0].length == 0) {
+        mydebug('bad user');
+        return res.status(400).send('email or password wrong');
+      } else if (results[0].length != 0) {
+        const sqlstring = JSON.stringify(results[0]);
+        const jsonstring = JSON.parse(sqlstring);
+        const dbpw = jsonstring[0]['password'];
+        const compareResult = bcrypt.compareSync(reqpw, dbpw);
+        if (compareResult) {
+          mydebug('succesful auth');
+          const token = jwt.sign(
+            {
+              name: jsonstring[0]['name'],
+              email: jsonstring[0]['email'],
+              isAdmin: jsonstring[0]['isAdmin'],
+              canAddGame: jsonstring[0]['canAddGame']
+            },
+            process.env.JWTKEY,
+            {
+              expiresIn: 5000
+            }
+          );
+          return res.status(200).send(token);
+        } else {
+          mydebug('bad password');
+          return res.status(400).send('email or password wrong');
+        }
+      }
+    });
+  } catch (error) {
+    mydebug(error.message);
+    return res.status(500).send('something went wrong on the backend...');
+  }
+});
+
+module.exports = router;
