269aad6fd421daee079fb7cc208da3d0653f446c
schwiddi <schwiddi@me.com>
2018-09-29 23:28:49 +0200

    removed buggy time in debug

--- common/mydebug.js
+++ common/mydebug.js
@@ -1,20 +1,5 @@
 // Hints
 /*jshint esversion: 6 */
-
-var currentdate = new Date();
-var datetime =
-  currentdate.getDate() +
-  '.' +
-  (currentdate.getMonth() + 1) +
-  '.' +
-  currentdate.getFullYear() +
-  ' ' +
-  currentdate.getHours() +
-  ':' +
-  currentdate.getMinutes() +
-  ':' +
-  currentdate.getSeconds();
-
-const mydebug = require('debug')(`app@${datetime}`);
+const mydebug = require('debug')(`app`);
 
 module.exports = mydebug;
