7ffd6bc33fa10cffad51f0454232217b8eae3091
schwiddi <schwiddi@me.com>
2018-09-29 22:47:25 +0200

    more logs

--- db/db_connection.js
+++ db/db_connection.js
@@ -5,12 +5,15 @@
 const mysql = require('mysql');
 const dbconfig = require('./db_conf');
 const mydebug = require('../common/mydebug');
+const log = require('../common/logger');
 
 // ENV things
 const db = mysql.createConnection(dbconfig);
 
 setInterval(function() {
   db.query('SELECT 1');
+  mydebug('made db call to hold connection alive...');
+  log.info('made db call to hold connection alive...');
 }, 600000);
 
 // now Export this Model
