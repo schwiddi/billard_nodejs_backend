a32bd7085d6c6f3c93fed6474a17c4c45f289455
Dominik Schwitter <dominik.schwitter@postfinance.ch>
2018-10-01 14:34:42 +0200

    added some debug

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,26 @@
+const mydebug = require('../common/mydebug');
 const jwt = require('jsonwebtoken');
 process.env.JWTKEY = 'tmp';
 
 module.exports = function auth(req, res, next) {
   const token = req.header('x-auth-token');
-  if (!token)
+  if (!token) {
+    mydebug('got req to protected endpoint without token');
     return res.status(401).send('you dont have enough privileges buddy......');
+  }
 
   try {
     const jwtpayload = jwt.verify(token, process.env.JWTKEY);
     if (jwtpayload) {
       req.user = jwtpayload;
+      mydebug('token succesful verified and loaded payload to req.user');
       next();
     } else {
-      res.status(400).send('hm...');
+      mydebug('hm....');
+      return res.status(400).send('hm...');
     }
   } catch (ex) {
-    res.status(400).send(ex.message);
+    mydebug('token could not be verified');
+    return res.status(400).send(ex.message);
   }
 };
