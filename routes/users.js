776e389bbd91ffb056221ddf3bdd81028dcb7f7c
schwiddi <schwiddi@me.com>
2018-10-06 02:08:16 +0200

    added protected get all users

--- routes/users.js
+++ routes/users.js
@@ -8,63 +8,90 @@ const mydebug = require('../common/mydebug');
 const _ = require('underscore');
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
+const auth = require('../middleware/auth');
 
 // setting up express
 const router = express.Router();
 
-
 // Input Validation for Users
 function validateUser(user) {
-    const schema = {
-        name: Joi.string().min(2).max(255).required(),
-        email: Joi.string().email({ minDomainAtoms: 2}).max(255).required(),
-        password: Joi.string().min(3).max(1024).required()
-    };
-    return Joi.validate(user, schema);
-  }
+  const schema = {
+    name: Joi.string()
+      .min(2)
+      .max(255)
+      .required(),
+    email: Joi.string()
+      .email({ minDomainAtoms: 2 })
+      .max(255)
+      .required(),
+    password: Joi.string()
+      .min(3)
+      .max(1024)
+      .required()
+  };
+  return Joi.validate(user, schema);
+}
+
+// get all users
+router.get('/', auth, async (req, res) => {
+  const sp = `CALL GetAllUsers()`;
+
+  db.query(sp, true, (error, results, fields) => {
+    if (error) {
+      return mydebug(error.message);
+    } else if (_.isEmpty(results[0])) {
+      mydebug(`no users in database...`);
+      return res.status(200).send('Currently no users in Database...');
+    } else {
+      mydebug('users listed');
+      return res.send(results[0]);
+    }
+  });
+});
 
 // write new user = register
 router.post('/', async (req, res) => {
-    const { error } = validateUser(req.body);
+  const { error } = validateUser(req.body);
+  if (error) {
+    mydebug(`joi user input validation was nok`);
+    return res.status(400).send(error.details[0].message);
+  }
+
+  // generate hashed pw here cause it needs async and i dont know how to do that inside de db if statement
+  const salt = await bcrypt.genSalt(10);
+  const hashedpw = await bcrypt.hash(req.body.password, salt);
+
+  const checkifmailalreadyregistered = `CALL CheckMailAlreadyRegistered('${
+    req.body.email
+  }')`;
+
+  db.query(checkifmailalreadyregistered, true, (error, results, fields) => {
     if (error) {
-      mydebug(`joi user input validation was nok`);
-      return res.status(400).send(error.details[0].message);
-    }
-
-    // generate hashed pw here cause it needs async and i dont know how to do that inside de db if statement
-    const salt = await bcrypt.genSalt(10);
-    const hashedpw = await bcrypt.hash(req.body.password, salt)
-
-
-    const checkifmailalreadyregistered = `CALL CheckMailAlreadyRegistered('${req.body.email}')`;
-
-    db.query(checkifmailalreadyregistered, true, (error, results, fields) => {
-      if (error) {
-        mydebug(error.message);
-        return res.status(500).send('something went wrong on the backend...');
-      } else if (_.isEmpty(results[0])) {
-        const sp = `CALL AddUser(
+      mydebug(error.message);
+      return res.status(500).send('something went wrong on the backend...');
+    } else if (_.isEmpty(results[0])) {
+      const sp = `CALL AddUser(
           '${req.body.name}',
           '${req.body.email}',
           '${hashedpw}')`;
-      
-        db.query(sp, true, (error, results, fields) => {
-          if (error) {
-            mydebug(error.message);
-            return res.status(500).send('something went wrong on the backend...');
-          } else if (_.isEmpty(results[0])) {
-            mydebug(`returned inserted user from db was empty`);
-            return res.status(500).send('something went wrong on the backend...');
-          } else {
-            mydebug('user added');
-            return res.send(results[0]);
-          }
-        });
-      } else {
-        mydebug('mail already registered');
-        return res.status(400).send('e-mail already registered...');
-      }
-    });
+
+      db.query(sp, true, (error, results, fields) => {
+        if (error) {
+          mydebug(error.message);
+          return res.status(500).send('something went wrong on the backend...');
+        } else if (_.isEmpty(results[0])) {
+          mydebug(`returned inserted user from db was empty`);
+          return res.status(500).send('something went wrong on the backend...');
+        } else {
+          mydebug('user added');
+          return res.send(results[0]);
+        }
+      });
+    } else {
+      mydebug('mail already registered');
+      return res.status(400).send('e-mail already registered...');
+    }
   });
+});
 
 module.exports = router;
