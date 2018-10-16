2aef4d11bbbfff9d1cbe32758ffe0ac3b5161825
schwiddi <34796865+schwiddi@users.noreply.github.com>
2018-10-16 20:32:05 +0200

    Create update_encounter_id_on_games.sql

---
+++ sql/encounter_patch/update_encounter_id_on_games.sql
@@ -0,0 +1,18 @@
+UPDATE games SET encounter_id = 2 WHERE encounter_id = 6;
+UPDATE games SET encounter_id = 4 WHERE encounter_id = 7;
+UPDATE games SET encounter_id = 5 WHERE encounter_id = 8;
+UPDATE games SET encounter_id = 10 WHERE encounter_id = 11;
+UPDATE games SET encounter_id = 9 WHERE encounter_id =12;
+UPDATE games SET encounter_id = 1 WHERE encounter_id = 13;
+UPDATE games SET encounter_id = 14 WHERE encounter_id = 15;
+UPDATE games SET encounter_id = 18 WHERE encounter_id = 19;
+UPDATE games SET encounter_id = 20 WHERE encounter_id = 21;
+UPDATE games SET encounter_id = 23 WHERE encounter_id = 24;
+UPDATE games SET encounter_id = 25 WHERE encounter_id = 26;
+UPDATE games SET encounter_id = 28 WHERE encounter_id = 29;
+UPDATE games SET encounter_id = 37 WHERE encounter_id = 38;
+UPDATE games SET encounter_id = 39 WHERE encounter_id = 40;
+UPDATE games SET encounter_id = 31 WHERE encounter_id = 41;
+UPDATE games SET encounter_id = 42 WHERE encounter_id = 43;
+UPDATE games SET encounter_id = 22 WHERE encounter_id = 46;
+UPDATE games SET encounter_id = 47 WHERE encounter_id = 49;
